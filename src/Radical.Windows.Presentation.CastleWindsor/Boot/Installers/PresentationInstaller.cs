e77ef84a224a232126c578bce57139de76b9ceb1
mauroservienti <mauro.servienti@gmail.com>
2021-05-24 16:18:08 +0200

    Add support for filtering scanned assemblies

--- src/Radical.Windows.Presentation.CastleWindsor/Boot/Installers/PresentationInstaller.cs
+++ src/Radical.Windows.Presentation.CastleWindsor/Boot/Installers/PresentationInstaller.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using Castle.Core;
+﻿using Castle.Core;
 using Castle.MicroKernel.Registration;
-using System.Linq;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
 
 namespace Topics.Radical.Windows.Presentation.Boot.Installers
 {
     /// <summary>
     /// A Windsor installer.
     /// </summary>
-    [Export( typeof( IWindsorInstaller ) )]
+    [Export(typeof(IWindsorInstaller))]
     public class PresentationInstaller : IWindsorInstaller
     {
-        IEnumerable<Type> GetServices( ComponentRegistration r ) 
+        IEnumerable<Type> GetServices(ComponentRegistration r)
         {
             var bf = System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public;
-            var property = r.GetType().GetProperties( bf ).Single( p => p.Name == "Services" );
-            var types = property.GetValue( r, null ) as IEnumerable<Type>;
+            var property = r.GetType().GetProperties(bf).Single(p => p.Name == "Services");
+            var types = property.GetValue(r, null) as IEnumerable<Type>;
 
             return types;
         }
@@ -27,55 +27,55 @@ namespace Topics.Radical.Windows.Presentation.Boot.Installers
         /// </summary>
         /// <param name="container">The container.</param>
         /// <param name="store">The configuration store.</param>
-        public void Install( Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store )
+        public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
         {
             var conventions = container.Resolve<BootstrapConventions>();
+            var types = container.Resolve<Type[]>();
+
             var currentDirectory = Helpers.EnvironmentHelper.GetCurrentDirectory();
 
             container.Register
             (
-				Types.FromAssemblyInDirectory( new AssemblyFilter( currentDirectory ).FilterByAssembly( conventions.IncludeAssemblyInContainerScan ) )
-                    .IncludeNonPublicTypes()
-                    .Where( t => conventions.IsViewModel( t ) && !conventions.IsExcluded( t ) )
-                    .WithService.Select( ( type, baseTypes ) => conventions.SelectViewModelContracts( type ) )
-                    .Configure( r =>
-                    {
-                        r.PropertiesIgnore( conventions.IgnoreViewModelPropertyInjection );
+                Types.From(types)
+                    .Where(t => conventions.IsViewModel(t) && !conventions.IsExcluded(t))
+                    .WithService.Select((type, baseTypes) => conventions.SelectViewModelContracts(type))
+                    .Configure(r =>
+                   {
+                       r.PropertiesIgnore(conventions.IgnoreViewModelPropertyInjection);
 
-                        var services = this.GetServices( r );
+                       var services = this.GetServices(r);
 
-                        if ( conventions.IsShellViewModel( services, r.Implementation) )
-                        {
-                            r.LifeStyle.Is( LifestyleType.Singleton );
-                        }
-                        else
-                        {
-                            r.LifeStyle.Is( LifestyleType.Transient );
-                        }
-                    } )
+                       if (conventions.IsShellViewModel(services, r.Implementation))
+                       {
+                           r.LifeStyle.Is(LifestyleType.Singleton);
+                       }
+                       else
+                       {
+                           r.LifeStyle.Is(LifestyleType.Transient);
+                       }
+                   })
             );
 
             container.Register
             (
-				Types.FromAssemblyInDirectory( new AssemblyFilter( currentDirectory ).FilterByAssembly( conventions.IncludeAssemblyInContainerScan ) )
-                    .IncludeNonPublicTypes()
-                    .Where( t => conventions.IsView( t ) && !conventions.IsExcluded( t ) )
-                    .WithService.Select( ( type, baseTypes ) => conventions.SelectViewContracts( type ) )
-                    .Configure( r =>
-                    {
-						r.PropertiesIgnore( conventions.IgnoreViewPropertyInjection );
+                Types.From(types)
+                    .Where(t => conventions.IsView(t) && !conventions.IsExcluded(t))
+                    .WithService.Select((type, baseTypes) => conventions.SelectViewContracts(type))
+                    .Configure(r =>
+                   {
+                       r.PropertiesIgnore(conventions.IgnoreViewPropertyInjection);
 
-                        var services = this.GetServices( r );
+                       var services = this.GetServices(r);
 
-                        if ( conventions.IsShellView( services, r.Implementation ) )
-                        {
-                            r.LifeStyle.Is( LifestyleType.Singleton );
-                        }
-                        else
-                        {
-                            r.LifeStyle.Is( LifestyleType.Transient );
-                        }
-                    } )
+                       if (conventions.IsShellView(services, r.Implementation))
+                       {
+                           r.LifeStyle.Is(LifestyleType.Singleton);
+                       }
+                       else
+                       {
+                           r.LifeStyle.Is(LifestyleType.Transient);
+                       }
+                   })
 
             );
         }
--- src/Radical.Windows.Presentation.CastleWindsor/Boot/WindsorApplicationBootstrapper.cs
+++ src/Radical.Windows.Presentation.CastleWindsor/Boot/WindsorApplicationBootstrapper.cs
@@ -1,26 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Hosting;
-using System.Linq;
-using System.Security.Principal;
-using System.Threading;
-using System.Windows;
-using Castle;
+﻿using Castle;
 using Castle.Facilities;
 using Castle.MicroKernel;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.Resolvers.SpecializedResolvers;
 using Castle.MicroKernel.SubSystems.Naming;
 using Castle.Windsor;
-using Topics.Radical.ComponentModel.Messaging;
-using Topics.Radical.Windows.Presentation.Messaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
 
 namespace Topics.Radical.Windows.Presentation.Boot
 {
     public class WindsorApplicationBootstrapper : ApplicationBootstrapper
     {
         IWindsorContainer container;
+        Func<string, AssemblyScanner.FilterResults> assemblyScannerFilter = null;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="WindsorApplicationBootstrapper"/> class.
@@ -30,16 +26,25 @@ namespace Topics.Radical.Windows.Presentation.Boot
 
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WindsorApplicationBootstrapper"/> class.
+        /// </summary>
+        /// <param name="assemblyScannerFilter">The filter delegate to include or exclude assemblies from the scanning operation.</param>
+        public WindsorApplicationBootstrapper(Func<string, AssemblyScanner.FilterResults> assemblyScannerFilter)
+        {
+            this.assemblyScannerFilter = assemblyScannerFilter;
+        }
+
         protected override IServiceProvider CreateServiceProvider()
         {
             var nss = new DelegateNamingSubSystem()
             {
-                SubSystemHandler = ( s, hs ) =>
+                SubSystemHandler = (s, hs) =>
                 {
-                    if( hs.Any( h => h.ComponentModel.IsOverridable() ) )
+                    if (hs.Any(h => h.ComponentModel.IsOverridable()))
                     {
-                        var nonOverridable = hs.Except( hs.Where( h => h.ComponentModel.IsOverridable() ) );
-                        if( nonOverridable.Any() )
+                        var nonOverridable = hs.Except(hs.Where(h => h.ComponentModel.IsOverridable()));
+                        if (nonOverridable.Any())
                         {
                             return nonOverridable.Single();
                         }
@@ -51,24 +56,40 @@ namespace Topics.Radical.Windows.Presentation.Boot
 
             this.container = new Castle.Windsor.WindsorContainer();
 
-            this.container.Kernel.AddSubSystem( SubSystemConstants.NamingKey, nss );
-            this.container.Kernel.Resolver.AddSubResolver( new ArrayResolver( this.container.Kernel, true ) );
+            this.container.Kernel.AddSubSystem(SubSystemConstants.NamingKey, nss);
+            this.container.Kernel.Resolver.AddSubResolver(new ArrayResolver(this.container.Kernel, true));
 
-            var wrapper = new ServiceProviderWrapper( this.container );
+            var wrapper = new ServiceProviderWrapper(this.container);
 
             var bootConventions = new BootstrapConventions();
 
             this.container.Register(
                 Component.For<IServiceProvider>()
-                    .Instance( wrapper )
-                    .PropertiesIgnore( pi => bootConventions.IgnorePropertyInjection( pi ) ) 
+                    .Instance(wrapper)
+                    .PropertiesIgnore(pi => bootConventions.IgnorePropertyInjection(pi))
             );
-            this.container.Register( Component.For<IWindsorContainer>().Instance( this.container ) );
+
+            var scanner = new AssemblyScanner();
+            if (assemblyScannerFilter != null)
+            {
+                scanner.AddAssemblyFilter(assemblyScannerFilter);
+            }
+
+            this.container.Register(Component.For<AssemblyScanner>().Instance(scanner));
+
+            var allTypes = scanner.Scan()
+                .SelectMany(assembly => assembly.GetTypes())
+                .Distinct()
+                .ToArray();
+
+            this.container.Register(Component.For<Type[]>().Instance(allTypes));
+
+            this.container.Register(Component.For<IWindsorContainer>().Instance(this.container));
             this.container.Register
             (
                 Component.For<BootstrapConventions>()
-                    .Instance( bootConventions )
-                    .PropertiesIgnore( pi => bootConventions.IgnorePropertyInjection( pi ) ) 
+                    .Instance(bootConventions)
+                    .PropertiesIgnore(pi => bootConventions.IgnorePropertyInjection(pi))
             );
 
             this.container.AddFacility<Castle.Facilities.SubscribeToMessageFacility>();
@@ -80,15 +101,15 @@ namespace Topics.Radical.Windows.Presentation.Boot
         [ImportMany]
         IEnumerable<IWindsorInstaller> Installers { get; set; }
 
-        protected override void OnCompositionContainerComposed( CompositionContainer container, IServiceProvider serviceProvider )
+        protected override void OnCompositionContainerComposed(CompositionContainer container, IServiceProvider serviceProvider)
         {
-            base.OnCompositionContainerComposed( container, serviceProvider );
+            base.OnCompositionContainerComposed(container, serviceProvider);
 
-            var toInstall = this.Installers.Where( i => this.ShouldInstall( i ) ).ToArray();
-            this.container.Install( toInstall );
+            var toInstall = this.Installers.Where(i => this.ShouldInstall(i)).ToArray();
+            this.container.Install(toInstall);
         }
 
-        protected virtual Boolean ShouldInstall( IWindsorInstaller installer )
+        protected virtual Boolean ShouldInstall(IWindsorInstaller installer)
         {
             return true;
         }
